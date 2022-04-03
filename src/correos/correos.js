cd99551d4ba8ddad60f68f3ff501dcc752ba327b
Hernan Enrique Gordillo Garzòn <Hernan Gordillo>
2022-04-02 20:55:26 -0500

    Envio de correos fix 4

--- src/correos/correos.js
+++ src/correos/correos.js
@@ -3,7 +3,9 @@ const log = require('../utils/logger/logger');
 
 // email sender 
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: "gmail",
+    host: 'smtp.gmail.com ',
+    port: 465,
     auth: {
         user: 'padonde6946@gmail.com',
         pass: 'PaDonde6946%'
