0f492b8a333fe1a4448d372cc4339c5c46a6cc39
Hernan Enrique Gordillo Garzòn <Hernan Gordillo>
2022-04-20 00:32:41 -0500

    Cors

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ const publicoPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicoPath));
 
 app.use( function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", req.header('Access-Control-Request-Headers'));
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 
     next();
 });
