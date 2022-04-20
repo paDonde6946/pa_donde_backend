dc17bca78c072c1bf73f80b1b1282e9b67f2a529
Hernan Enrique Gordillo Garzòn <Hernan Gordillo>
2022-04-19 23:53:03 -0500

    Cors

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ require('./src/sockets/socket');
 const publicoPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicoPath));
 
+app.use( function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    next();
+});
 
 // Rutas Movil 
 app.use('/app/login', require('./src/rutas/sesionApp_ruta.js'));
